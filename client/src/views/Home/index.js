1ef1fa4078b5988266133c1e22d4882cb4a21e25
krystianhuang <krystianhuang@gmail.com>
2022-05-12 01:07:08 +0800

    feat: Update home page

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -30,15 +30,20 @@ const Home = () => {
             <div className='feature'>
               <img src={feature1} className='feature-img' />
               <div className='text-wrapper'>
-                <h2 className='feature-title'>Recommendation function</h2>
+                <h2 className='feature-title'>Chat function</h2>
                 <div className='text'>
-                  The system will recommend friends of all related hobbies to
-                  the user according to the user's hobbies.
+                  User can communicate with each other in real time.
                 </div>
-
                 <div className='text'>
-                  If the user is not interested in this recommended friend, user
-                  can delete it. The system will never recommend this friend.
+                  User can send emojis when chatting based on the Emoji package.
+                </div>
+                <div className='text'>
+                  User can send their favourite pictures(png/jpeg) through local
+                  upload.
+                </div>
+                <div className='text'>
+                  User can delete a single chat history (The single chat history
+                  of both chatters will be cleared).
                 </div>
               </div>
             </div>
